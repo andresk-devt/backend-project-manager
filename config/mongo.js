e0526db7ee68907f1d7d3a2b3ab4440a22b54703
andresk-devt <learningxandres@gmail.com>
2023-12-10 00:04:12 -0500

    Organice the data base conection and install the chalk dependence to coloring the console prints

---
+++ config/mongo.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const chalk = require("chalk");
+
+const dbConnect = async () => {
+  const DB_URI = process.env.DB_URI;
+
+  try {
+    await mongoose.connect(DB_URI);
+    console.log(chalk.bgGreen("Connected to MongoDB"));
+  } catch (err) {
+    console.log(chalk.bgRed("Error when trying to connect to the database", err));
+  }
+};
+
+module.exports = dbConnect;
