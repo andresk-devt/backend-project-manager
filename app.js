19b946bbc587bcc90f24cc90a456b52a3c553fc4
andresk-devt <learningxandres@gmail.com>
2024-01-08 12:25:17 -0500

    Configure the comand dev to run the project

--- app.js
+++ app.js
@@ -6,13 +6,12 @@ const dbConnect = require('./config/mongo');
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use("/api", require('./routes'))
 
 const PORT = process.env.PORT || 3000;
 
-app.use("/api", require('./routes'))
+dbConnect();
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port http://localhost:${PORT}/`)
 });
-
-dbConnect();
